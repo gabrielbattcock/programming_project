f0f1ad9f4f98df31ee9f692843f498b23d577944
Gabriel Batcock <gabrielbattcock@gmail.com>
2022-11-14 19:59:41 +0000

    Monday night commit

--- project_code.R
+++ project_code.R
@@ -6,8 +6,10 @@ setwd(working_directory)
 library(arsenal)
 library(tidyverse)
 library(Epi)
+library(ggplot2)
 
 #read both of the files saved in the same working directory
+
 path_to_file <- "births.csv"
 births <- read_csv(path_to_file)
 
@@ -16,24 +18,28 @@ score <- read_csv(path_to_file)
 head(births)
 
 #refactor the categorical variables with 
+
 births$lowbw <- factor(births$lowbw, labels = c("No","Yes"))
 births$preterm <- factor(births$preterm, labels = c("No","Yes"))
 births$hyp <- factor(births$hyp, labels = c('No', "Yes"))
+
 #sex is 1=M, 2=F from the codebook
 births$sex <- factor(births$sex, labels = c("Male", "Female"))
 
 #remove any null values
+
 births <- na.omit(births)
 head(births)
 
 #merge the births data set with the score keeping both the matched and unmatched data
+
 births <- merge(births, score, by = "id", all = TRUE)
 births <- births[order(births$score),]
 head(births)
 
 #create plots to examine whether a spearman is valid
 
-par(mfrow=c(2,3))
+par(mfrow=c(3,2))
 plot(births$bweight~births$gestwks)
 plot(births$bweight~births$matage)
 plot(births$bweight~births$score)
@@ -46,24 +52,55 @@ plot(births$score~births$matage)
 
 #create correlation matrix between all continuous variables
 
-corr <- cor(births[,c("bweight", "gestwks", "score")], use = "complete.obs", method = "spearman")
+corr <- cor(births[,c("bweight", "gestwks", "score")], use = "complete.obs")
 print(corr)
 
-# two_way <- lm(data = births_match, formula = )
+#two way frequency tables for categorical variables
+
+lowbw_preterm <- with(births, table(lowbw, preterm))
+sprintf("Two way frequency table between low birth weight and a preterm baby: ")
+print(lowbw_preterm)
+
+sex_bw <- with(births, table(lowbw, sex))
+print("Two way frequency table between low birth weight and sex: ")
+print(sex_bw)
+
+hyp_bw <- with(births, table(lowbw, hyp))
+print("Two way frequency table between low birth weight and hypertension: ")
+print(hyp_bw)
+
+preterm_hyp <- with(births, table(hyp, preterm))
+print("Two way frequency table between a preterm baby and hypertension: ")
+print(preterm_hyp)
+
+preterm_sex <- with(births, table(sex, preterm))
+print("Two way frequency table between a preterm baby and sex: ")
+print(preterm_sex)
+
+sex_hyp <- with(births, table(hyp, sex))
+print("Two way frequency table between hypertension and sex: ")
+print(sex_hyp)
   
 #create new column with a Yes for score > 150
-births$highscore <- with(births, ifelse(score>150, "Yes", "No"))
-head(births)
 
+births$highscore <- with(births, ifelse(score>150, "Yes", "No"))
+
+#check the tail to see that NAs have been kept as NA
+
+tail(births)
+
+#create aggregated dataset with mean birthweight by sex and highscore
 
 aggr <- aggregate(bweight~highscore+sex, data=births, FUN=mean)
 print(aggr)
 
 #create a plot between the variables bweight and gestwks as they are the only cont. variables with a corr >0.5
 
-plot(x=births$gestwks, y=births$bweight)
-
-
+ggplot(births, aes(x = gestwks, y = bweight)) +
+      geom_point(color = "#000099", alpha = 0.3)+ 
+      geom_smooth(method = "lm", se = FALSE, color = "#CC0000") +
+      labs(x = "Gestational weeks", y = "Birth weight (g)")
+ggsave("bweight_plot_r.pdf")
 
 #write the final data to a csv
 
--- python_script.py
+++ python_script.py
@@ -1,17 +1,11 @@
-#this is a python script for my project
+#Programming assessment 2022
+#candidate number: 
+#python code
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt 
 
-#define any functions
-def highscore(score):
-    if score >= 150:
-        return "Yes"
-    elif score == np.nan:
-        return np.nan
-    else:
-        return "No"
 
 
 #import the files from the current working directory and store them as DF
@@ -30,28 +24,74 @@ births['lowbw'] = births['lowbw'].map({1:"Yes",0:"No"})
 births['preterm'] = births['preterm'].map({1:"Yes",0:"No"})
 births['hyp'] = births['hyp'].map({1:"Yes",0:"No"})
 births['sex'] = births['sex'].map({1:"Male",2:"Female"})
+print("Check that the categorical data has been transformed: \n", births.head())
 
 #remove NA results
 births = births.dropna()
+print("Check that the third value with NA has been dropped: \n", births.head())
 
 #merge the births data with the score 
 births = pd.merge(births, score, how = 'outer')
+print("The new data set comprising both the trasnformed births data and the score: \n",
+    births.head(), "\n",
+    births.tail()
+)
+
 #reordering by score ascending
 births = births.sort_values('score')
+print("Check that the head is ordered by score: \n", births.head())
+
 
 #check the correlation between continuous variables
-
 corr_births = births[['bweight','gestwks','matage','score']].copy()
-correlation = corr_births.corr(method = 'spearman')
+correlation = corr_births.corr()
 
-print(correlation)
-print(births)
+print("Presented is a correlation matrix for the continuous variables of the dataset: \n", correlation, "\n")
 
-######################## TO DO 
-######################## two way dist of cat vars
+#two way distribution of cat vars
+
+sex_bw = pd.crosstab(index = births["lowbw"], columns = births["sex"])
+print("Two way frequency table between low birth weight and sex: \n", sex_bw, "\n")
+
+preterm_bw = pd.crosstab(index = births["lowbw"], columns = births["preterm"])
+print("Two way frequency table between low birth weight and a preterm baby: \n", preterm_bw, "\n")
+
+hyp_bw = pd.crosstab(index = births["lowbw"], columns = births["hyp"])
+print("Two way frequency table between low birth weight and hypertension: \n", hyp_bw, "\n")
+
+preterm_hyp = pd.crosstab(index = births["hyp"], columns = births["preterm"])
+print("Two way frequency table between a preterm baby and hypertension: \n", preterm_hyp, "\n")
+
+preterm_sex = pd.crosstab(index = births["preterm"], columns = births["sex"])
+print("Two way frequency table between a preterm baby and sex: \n", preterm_sex, "\n")
+
+sex_hyp = pd.crosstab(index = births["hyp"], columns = births["sex"])
+print("Two way frequency table between hypertension and sex: \n", sex_hyp, "\n")
+
+#create a new binary column for a score of >150
+#need to keep the NaNs in so that teh aggregate calculation is correct
+births["highscore"] = births['score'].apply(lambda x: np.nan if pd.isnull(x) else "Yes" if x >= 150 else "No")
+print(births.head(),'\n', births.tail())
+
+#aggreagated version of data set with average birthweight by highscore and sex
+aggregate = births.groupby(['sex','highscore']).agg({'bweight':'mean'})
+print("Mean birth weight by sex and highscore : \n", aggregate)
+
+#create a plot of bweight against gestwks as these are the only variables with corr> 0.5
+
+bweight = births['bweight'].to_numpy()
+gestwks = births['gestwks'].to_numpy()
+fig, ax = plt.subplots()
+plt.grid()
+ax.scatter(gestwks, bweight, alpha=0.5)
+ax.set_xlabel('Gestational weeks')
+ax.set_ylabel('Birth weight (g)')
+ax.set_title('')
+plt.show()
+fig.savefig("bweight_plot_py.pdf",dpi=fig.dpi)
 
 
-births["highscore"] = births['score'].apply(highscore)
-print(births.tail())
-
+#save the final dataset
+filepath = 'births_output_py.csv'
+births.to_csv(filepath)  
 
